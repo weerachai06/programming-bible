eb00654b40a4857ca1fdf0a6844c35a88d1d935f
Weerachai Plodkaew <clkeen157@gmail.com>
2024-11-09 04:36:54 +0700

    wip: add sticky example

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -9,6 +10,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    stickyStack: {
+      0: "0",
+      1: "1rem",
+      2: "2rem",
+      3: "3rem",
+      4: "4rem",
+    },
+    stickyOffset: {
+      0: "0",
+      1: "1rem",
+      2: "2rem",
+      3: "3rem",
+      4: "4rem",
+      5: "5rem",
+      6: "6rem",
+      7: "7rem",
+      8: "8rem",
+      9: "9rem",
+      10: "10rem",
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -16,6 +37,42 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme, addBase, addUtilities }) {
+      addBase({
+        ":root": {
+          "--header": "56px",
+          "--sticky-base": "0",
+          "--sticky-height": "4rem",
+          counterReset: "sticky-stack",
+        },
+      });
+      // Container and element utilities
+      addUtilities({
+        ".sticky-container": {
+          contentVisibility: "auto",
+          containIntrinsicSize: "auto",
+        },
+        ".sticky-element": {
+          position: "sticky",
+          counterIncrement: "sticky-stack",
+          top: "calc(var(--sticky-base) + (counter(sticky-stack) - 1) * var(--sticky-height))",
+        },
+      });
+      // Match utilities for custom sticky offsets
+      matchUtilities(
+        {
+          "sticky-height": (value) => ({
+            "--sticky-height": value,
+          }),
+          "sticky-offset": (value) => ({
+            position: "sticky",
+            top: value,
+          }),
+        },
+        { values: theme("stickyStack") }
+      );
+    }),
+  ],
 };
 export default config;
